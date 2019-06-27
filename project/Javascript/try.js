8bace1073d98ef0e9e224940becad1fdd30f16f3
shraddhansu panda <47125559+shraddhansupanda@users.noreply.github.com>
2019-06-27 21:38:14 +0530

    Add files via upload

--- project/Javascript/try.js
+++ project/Javascript/try.js
@@ -8,6 +8,22 @@ var x6 = parseInt(document.getElementById("6th").value);
 var x7 = parseInt(document.getElementById("7th").value);
 var x8 = parseInt(document.getElementById("8th").value);
 var x9 = parseInt(document.getElementById("9th").value);
-k=x1+x2;
-document.getElementById("msg").innerHTML = k;
-}
+var sum=15
+  if(x1!="" && x2!="" && x3!="" && x4!="" && x5!="" && x6!="" && x7!="" && x8!="" && x9!=""){
+    if(x1!=x2 && x1!=x3 && x1!=x4 && x1!=x5 && x1!=x6 && x1!=x7 && x1!=x8 && x1!=x9 && x2!=x3 && x2!=x4 && x2!=x5 && x2!=x6 && x2!=x7 && x2!=x8 && x2!=x9 && x3!=x4 && x3!=x5 && x3!=x6 && x3!=x7 && x3!=x8 && x3!=x9 && x4!=x5 && x4!=x6 && x4!=x7 && x4!=x8 && x4!=x9 && x5!=x6 && x5!=x7 && x5!=x8 && x5!=x9 && x6!=x7 && x6!=x8 && x6!=x9 && x7!=x8 && x7!=x9 && x8!=x9  ){
+        if(x1+x2+x3==sum && x4+x5+x6==sum && x7+x8+x9==sum && x1+x4+x7==sum && x2+x5+x8==sum && x3+x6+x9==sum && x1+x5+x9==sum && x3+x5+x7==sum){
+            document.getElementById("msg").innerHTML ="This is a magic square !";
+        }
+        else{
+            document.getElementById("msg").innerHTML ="This is not a magic square";
+         }
+    }
+    else{
+        document.getElementById("msg").innerHTML ="You cannot use the same number twice :)";
+    }
+ }
+ else{
+     document.getElementById("msg").innerHTML ="there is a empty box,please try again"
+ }
+
+}   
