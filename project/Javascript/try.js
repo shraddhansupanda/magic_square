3c8a333e9bee947e209782b79a82d93e257e6a34
shraddhansu panda <47125559+shraddhansupanda@users.noreply.github.com>
2019-06-27 06:50:48 +0530

    Add files via upload

---
+++ project/Javascript/try.js
@@ -0,0 +1,13 @@
+function magic() {
+var x1 = parseInt(document.getElementById("1st").value);
+var x2 = parseInt(document.getElementById("2nd").value);
+var x3 = parseInt(document.getElementById("3rd").value);
+var x4 = parseInt(document.getElementById("4th").value);
+var x5 = parseInt(document.getElementById("5th").value);
+var x6 = parseInt(document.getElementById("6th").value);
+var x7 = parseInt(document.getElementById("7th").value);
+var x8 = parseInt(document.getElementById("8th").value);
+var x9 = parseInt(document.getElementById("9th").value);
+k=x1+x2;
+document.getElementById("msg").innerHTML = k;
+}
